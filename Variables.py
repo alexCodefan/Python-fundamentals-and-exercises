d8fe2bb7c859c9e5a295004c525dab30233c8f34
Alexander Rey <ajreyv@gmail.com>
2026-02-14 07:11:03 -0500

    Add variables basic examples

---
+++ Variables.py
@@ -0,0 +1,19 @@
+#Topic: Variables in Python
+#Description: Basic examples of variable declaration and usage
+
+name = "Lisa"
+age = 25
+height = 1.70
+is_women = True
+
+print("Name: ", name)
+print("Age: ", age)
+print("Height: ", height)
+print("Is a women: ", is_women)
+
+print("-------------------")
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_women))
