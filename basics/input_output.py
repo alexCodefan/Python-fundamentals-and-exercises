df3f401fcbc7a4894bf397c18807ab0b59747aaa
Alexander Rey <ajreyv@gmail.com>
2026-02-14 08:37:50 -0500

    Add input output simple exercise

---
+++ basics/input_output.py
@@ -0,0 +1,11 @@
+#Topic: Input and output
+#Description: Basic usage of input() and print()
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+heigh = input("Enter your heigh: ")
+
+print(f"Hello {name}, you are {age} years old and you are {heigh} metres tall")
+
+
+
